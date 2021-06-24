e352cacf1b3251582cac08f8d016536a626c6c9b
agustin <agustinagapito09@gmail.com>
2021-06-24 16:25:42 +0800

    try

--- src/pages/Watch/Watch.js
+++ src/pages/Watch/Watch.js
@@ -148,12 +148,12 @@ export default function Watch() {
                                 ref.current.seek(seekTime)
                                 console.log(ref.current)
                             }}
-                            // onLoadedData={() => ref.current.actions.play()}
+                            onLoadedData={() => ref.current.actions.play()}
                             // onReady={ () =>ref.current.actions.play()}
                             // onCanPlay={ () => ref.current.video.toggleFullscreen()}
                             // onCanPlay={(e) => console.log(':Asdfasfdf')}
                             playsInline 
-                            autoPlay={true}
+                            // autoPlay={true}
                             onPlaying={()=>ref.current.video.toggleFullscreen()}
                             fullscreen={()=>console.log("adfsdf")}
                             onPlay={handlePlay}
