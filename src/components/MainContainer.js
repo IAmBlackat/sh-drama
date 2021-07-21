de5beb39076722889c07dd5f4eb108a8ca91bedc
agustin <agustinagapito09@gmail.com>
2021-07-21 14:10:07 +0800

    new source url

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,5 +1,5 @@
 import { Container, makeStyles } from '@material-ui/core'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Appbar } from './Appbar'
 import Content from './Content'
 import { Sidenav } from './Sidenav'
@@ -14,31 +14,9 @@ const styles = makeStyles( (theme) => ({
 
 export default function MainContainer() {
     const classes = styles()
-    useEffect( () => {
-        axios.get('http://localhost:5000/cdn')
-        .then( res => console.log(res.data))
-    }, [])
-    // var playerInstance = jwplayer("myVideo");
-    // var countplayer = 1;
-    // var countcheck = 0;
-    // var current_time  = 0;
-    // playerInstance.setup({
-    //     sources:[{file: 'https://storage.googleapis.com/sacred-entry-315002/W3VKETS_ZPF2/22a_1624550408_4584.mp4',label: 'HD P','type' : 'mp4'}]
-    // }); https://andhaetg.github.io/plyr/env.js
-
     return (
         <Container maxWidth="xl" >
             <Router>
-                <iframe 
-                    src="https://animixplay.to/api/liveTkRVNE5BPT1MVFhzM0dyVTh3ZTlPVGtSVk5FNUJQVDA9"
-                    
-                />
-                {/* <video 
-                    src="https://storage.googleapis.com/sacred-entry-315002/W3VKETS_ZPF2/22a_1624550408_4584.mp4"
-                    width="100%"
-                    height={400}
-                    controls
-                /> */}
                 <Appbar />
                 
                 <div className={classes.section} >
